11f5ede60a35ef73062d2ac6c6b449b9f9b95351
Patrick Ferguson <patrick@ferguson.cloud>
2021-06-15 01:56:53 +1000

    Improve readme and add new icon version and API rate limit calculations.

--- README.md
+++ README.md
@@ -6,15 +6,23 @@ Offers customisable prefixes to match the translation style and language.
 Add this bot to your Discord server:
 https://discord.com/oauth2/authorize?client_id=853320365514031155&scope=bot+applications.commands
 
+If you find this bot useful, please consider supporting at:
+https://ko-fi.com/oatycreates
+
+Thanks as always to the translators ❤
+
+Author: Oats - @OatyCreates ©2021
+
+---
+
 To run the bot locally (needs Node/npm installed): npm install && node .
+
+To deploy to Google App Engine (once configured): gcloud app deploy
+
+---
 
 Command examples:
 
 !tlwatch https://www.youtube.com/watch?v=###########
 !tlstop
-!tlprefix [EN] EN:
-
-If you find this bot useful, please consider supporting at:
-https://ko-fi.com/oatycreates
-
-Author: Oats - @OatyCreates ©2021
+!tlprefix [ES] ES:
--- index.js
+++ index.js
@@ -9,14 +9,26 @@
  * Add this bot to your Discord server:
  * https://discord.com/oauth2/authorize?client_id=853320365514031155&scope=bot+applications.commands
  *
+ * If you find this bot useful, please consider supporting at:
+ * https://ko-fi.com/oatycreates
+ *
+ * Thanks as always to the translators ❤
+ *
+ * Author: Oats - @OatyCreates ©2021
+ *
+ * ---
+ *
  * To run the bot locally (needs Node/npm installed): npm install && node .
  *
  * To deploy to Google App Engine (once configured): gcloud app deploy
  *
- * If you find this bot useful, please consider supporting at:
- * https://ko-fi.com/oatycreates
+ * ---
  *
- * Author: Oats - @OatyCreates ©2021
+ * Command examples:
+ *
+ * !tlwatch https://www.youtube.com/watch?v=###########
+ * !tlstop
+ * !tlprefix [EN] EN:
  */
 
 // Set up environment variables
